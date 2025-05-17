a83dfa239b898d45bd4d6b1b70d5971f3e873414
LHoG <1476261+lhog@users.noreply.github.com>
2025-05-17 23:41:38 +0200

    Misc

--- rts/Rendering/Textures/TextureFormat.h
+++ rts/Rendering/Textures/TextureFormat.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include <cstdint>
-#include "Rendering/GL/myGL.h"
 
 namespace GL
 {
@@ -13,6 +12,6 @@ namespace GL
 	uint32_t GetExternalFormatFromInternalFormat(uint32_t internalFormat);
 	uint32_t GetExternalFormatFromNumChannels(uint32_t numChannels);
 	uint32_t GetDataTypeSize(uint32_t glDataType);
-	uint32_t GetBindingQueryFromTarget(GLenum target);
+	uint32_t GetBindingQueryFromTarget(uint32_t target);
 	uint32_t GetCompressedInternalFormat(uint32_t internalFormat);
 }
