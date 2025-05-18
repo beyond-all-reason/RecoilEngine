bd074155a3e5614425688a39fbd6f2a4cb95eb26
LHoG <1476261+lhog@users.noreply.github.com>
2025-05-18 21:22:45 +0200

    ++

--- rts/Rendering/GL/State.cpp
+++ rts/Rendering/GL/State.cpp
@@ -4,5 +4,6 @@ decltype(GL::State::Attributes) GL::State::Attributes;
 
 void GL::myGL::PixelStoreUnpackAlignment(GLint alignment)
 {
+	assert(0 < alignment && alignment <= 4);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, alignment);
 }
