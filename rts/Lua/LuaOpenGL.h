c59b77e4f88b278d4289671242dca652bca15e15
Saurtron <saurtron@riseup.net>
2025-05-26 17:03:57 +0200

    Revert "Add setter function for PointSmooth."
    This reverts commit fd51d0e4d306c4d8aea498a6a2103af378321093.

--- rts/Lua/LuaOpenGL.h
+++ rts/Lua/LuaOpenGL.h
@@ -221,7 +221,6 @@ class LuaOpenGL {
 
 		static int LineWidth(lua_State* L);
 		static int PointSize(lua_State* L);
-		static int PointSmooth(lua_State* L);
 		static int PointSprite(lua_State* L);
 		static int PointParameter(lua_State* L);
 
