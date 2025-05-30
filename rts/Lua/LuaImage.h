7b6f2f5e2877c7da8cc54e3aca7dd6f5ff41b9ea
Saurtron <saurtron@riseup.net>
2025-05-30 18:44:18 +0200

    Tab header file.

--- rts/Lua/LuaImage.h
+++ rts/Lua/LuaImage.h
@@ -13,45 +13,45 @@ struct lua_State;
 class CBitmap;
 
 class LuaImageData {
-public:
-	LuaImageData(std::string filename, int channels, int dataType, bool luminance);
+	public:
+		LuaImageData(std::string filename, int channels, int dataType, bool luminance);
 
-	std::string filename;
-	bool valid;
-	std::shared_ptr<CBitmap> bitmap;
-	unsigned int width;
-	unsigned int height;
-	unsigned int channels;
+		std::string filename;
+		bool valid;
+		std::shared_ptr<CBitmap> bitmap;
+		unsigned int width;
+		unsigned int height;
+		unsigned int channels;
 };
 
 class LuaImage {
-public:
-	LuaImage() { }
+	public:
+		LuaImage() { }
 
-private: // helpers
-	static bool CreateMetatable(lua_State* L);
+	private: // helpers
+		static bool CreateMetatable(lua_State* L);
 
-private: // metatable methods
-	static int meta_gc(lua_State* L);
-	static int meta_index(lua_State* L);
-	static int meta_tostring(lua_State* L);
+	private: // metatable methods
+		static int meta_gc(lua_State* L);
+		static int meta_index(lua_State* L);
+		static int meta_tostring(lua_State* L);
 
-public:
-	static bool PushEntries(lua_State* L);
+	public:
+		static bool PushEntries(lua_State* L);
 
-private: // call-outs
-	static int LoadImage(lua_State* L);
-	static int DeleteImage(lua_State* L);
-	static int ReadPixel(lua_State* L);
-	static int ReadMapPixel(lua_State* L);
-	static int GetFormat(lua_State* L);
-public:
-	static const char* DataTypeToString(int dataType);
-	static int StringToDataType(const char* str);
-	static const char* ChannelsToFormat(int channels);
-	static int FormatToChannels(const char* format);
-	static int ParseFormat(lua_State* L, int index);
-	static int ParseDataType(lua_State* L, int index);
+	private: // call-outs
+		static int LoadImage(lua_State* L);
+		static int DeleteImage(lua_State* L);
+		static int ReadPixel(lua_State* L);
+		static int ReadMapPixel(lua_State* L);
+		static int GetFormat(lua_State* L);
+	public:
+		static const char* DataTypeToString(int dataType);
+		static int StringToDataType(const char* str);
+		static const char* ChannelsToFormat(int channels);
+		static int FormatToChannels(const char* format);
+		static int ParseFormat(lua_State* L, int index);
+		static int ParseDataType(lua_State* L, int index);
 };
 
 extern LuaImage luaImage;
