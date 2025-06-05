cffc5cc08d881c1f26091300aa7e37d955b0be83
Saurtron <saurtron@riseup.net>
2025-06-06 00:59:32 +0200

    Make AllowInterplayerSecrets a modoption instead.
    - this way all players know about the config automatically and can be configured per game.

--- rts/Net/GameServer.cpp
+++ rts/Net/GameServer.cpp
@@ -75,7 +75,6 @@ CONFIG(bool, WhiteListAdditionalPlayers).defaultValue(true);
 CONFIG(bool, ServerRecordDemos).defaultValue(false).dedicatedValue(true);
 CONFIG(bool, ServerLogInfoMessages).defaultValue(false);
 CONFIG(bool, ServerLogDebugMessages).defaultValue(false);
-CONFIG(bool, AllowInterplayerSecrets).defaultValue(false);
 CONFIG(std::string, AutohostIP).defaultValue("127.0.0.1");
 
 
@@ -164,7 +163,7 @@ void CGameServer::Initialize()
 	whiteListAdditionalPlayers = configHandler->GetBool("WhiteListAdditionalPlayers");
 	logInfoMessages = configHandler->GetBool("ServerLogInfoMessages");
 	logDebugMessages = configHandler->GetBool("ServerLogDebugMessages");
-	allowInterplayerSecrets = configHandler->GetBool("AllowInterplayerSecrets");
+	allowInterplayerSecrets = myGameSetup->interplayerSecrets;
 
 	rng.Seed((myGameData->GetSetupText()).length());
 
