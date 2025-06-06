a0fc5c64815cfd755e7cebb23f0908d38c989e92
Saurtron <saurtron@riseup.net>
2025-06-06 02:34:09 +0200

    Remove unused function from header.

--- rts/lib/lua/include/lua_privileges.h
+++ rts/lib/lua/include/lua_privileges.h
@@ -3,7 +3,6 @@
 #ifndef lua_privileges_h
 #define lua_privileges_h
 
-LUALIB_API int (luaL_loadfile_privileged) (lua_State *L, const char *filename);
 LUALIB_API int (luaL_loadbuffer_privileged) (lua_State *L, const char *buff, size_t sz,
                                   const char *name, bool privileged);
 LUA_API int   (lua_load_privileged) (lua_State *L, lua_Reader reader, void *dt,
