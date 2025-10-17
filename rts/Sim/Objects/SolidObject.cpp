4443569ab8f1ca9c86e7d54e0a75efd08362b05b
LHoG <1476261+lhog@users.noreply.github.com>
2025-10-17 13:07:56 +0200

    Cleanup

--- rts/Sim/Objects/SolidObject.cpp
+++ rts/Sim/Objects/SolidObject.cpp
@@ -1,15 +1,4 @@
-#include "SolidObject.h"
-#include "SolidObject.h"
-#include "SolidObject.h"
-#include "SolidObject.h"
-#include "SolidObject.h"
-#include "SolidObject.h"
-#include "SolidObject.h"
-#include "SolidObject.h"
-#include "SolidObject.h"
-#include "SolidObject.h"
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
-
 
 #include "SolidObject.h"
 #include "SolidObjectDef.h"
