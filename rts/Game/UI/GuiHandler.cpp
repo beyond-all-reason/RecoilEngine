a9f564e6828eb381847dcd38cfbab56cc93ac6a8
LHoG <1476261+lhog@users.noreply.github.com>
2025-10-26 21:54:46 +0100

    Explain why BindIconTexByString is not implemented

--- rts/Game/UI/GuiHandler.cpp
+++ rts/Game/UI/GuiHandler.cpp
@@ -2796,6 +2796,11 @@ static inline bool BindIconTexByString(const std::string& str)
 }
 */
 
+// the archaic code of gui handler was not designed to work with atlased textures
+// like icons are since https://github.com/beyond-all-reason/RecoilEngine/pull/2604 was merged
+// GuiHandler should eventually be replaced by something better,
+// so at the moment I don't feel like fixing it
+// return false and assert instead
 static inline bool BindIconTexByString(const std::string& str)
 {
 	assert(false);
