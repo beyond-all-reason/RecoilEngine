eead0c7151eed89adf27d52f6c34337b89f17030
LHoG <1476261+lhog@users.noreply.github.com>
2025-11-09 21:00:36 +0100

    Remove the console font assignment inside Recoil::InitConsole()

--- rts/System/Platform/ConsoleInit.cpp
+++ rts/System/Platform/ConsoleInit.cpp
@@ -12,12 +12,5 @@ void Recoil::InitConsole()
 #ifdef _WIN32
 	SetConsoleOutputCP(CP_UTF8);
 	SetConsoleCP(CP_UTF8);
-
-	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_FONT_INFOEX cfie = { 0 };
-	cfie.cbSize = sizeof(cfie);
-	lstrcpyW(cfie.FaceName, L"Lucida Console");
-	cfie.dwFontSize.Y = 13;
-	SetCurrentConsoleFontEx(hStdOut, FALSE, &cfie);
 #endif // _WIN32
 }
