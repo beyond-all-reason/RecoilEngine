865d23e61bed5bd6c17104caf76afd8d27089a65
LHoG <1476261+lhog@users.noreply.github.com>
2025-11-09 20:36:40 +0100

    Remove questionable setvbuf(stdout, nullptr, ...) calls

--- rts/System/Platform/ConsoleInit.cpp
+++ rts/System/Platform/ConsoleInit.cpp
@@ -1,6 +1,7 @@
 #include "ConsoleInit.hpp"
 
 #include <stdio.h>
+#include <sys/stat.h>
 
 #ifdef _WIN32
 	#include <windows.h>
@@ -11,8 +12,7 @@ void Recoil::InitConsole()
 #ifdef _WIN32
 	SetConsoleOutputCP(CP_UTF8);
 	SetConsoleCP(CP_UTF8);
-	setvbuf(stdout, nullptr, _IOFBF, 8192);
-	setvbuf(stderr, nullptr, _IONBF, 0);
+
 	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_FONT_INFOEX cfie = { 0 };
 	cfie.cbSize = sizeof(cfie);
