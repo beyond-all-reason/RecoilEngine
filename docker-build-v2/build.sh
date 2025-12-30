1773c62b28123b0933bfa0649bdfc0aba2e32d77
Marek Rusinowski <marekrusinowski@gmail.com>
2025-12-30 12:33:03 +0100

    Add support of podman container runtime and autodetection

--- docker-build-v2/build.sh
+++ docker-build-v2/build.sh
@@ -68,7 +68,21 @@ cd "$(dirname "$(readlink -f "$0")")/.."
 source docker-build-v2/images_versions.sh
 mkdir -p build-$OS .cache/ccache-$OS
 
-RUNTIME=${CONTAINER_RUNTIME:-docker}
+# Detect and select container runtime. Support explicit override, docker and
+# podman, with docker being the default as that's likely more expected behavior.
+if [[ -n "${CONTAINER_RUNTIME:-}" ]]; then
+    RUNTIME="$CONTAINER_RUNTIME"
+elif command -v docker &> /dev/null &&
+     # We verify the output of docker version to detect podman-docker package
+     # and aliases from podman to docker people might have.
+     ! docker version | grep -qi podman; then
+    RUNTIME=docker
+elif command -v podman &> /dev/null; then
+    RUNTIME=podman
+else
+    echo "Neither docker nor podman is installed. Please install one of them."
+    exit 1
+fi
 
 # Use locally build image if available, and pull from upstream if not
 image=recoil-build-$PLATFORM:latest
