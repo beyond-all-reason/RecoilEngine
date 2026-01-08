2c0ae17984e1f5570c0c5d1bfb5249d14c8ca609
LHoG <1476261+lhog@users.noreply.github.com>
2026-01-08 20:57:33 +0100

    Add additional ForwardSlashes pass in FileSystem::GetNormalizedPath() to make weird Windows paths output the same normalized result on Linux and Windows

--- rts/System/FileSystem/FileSystem.cpp
+++ rts/System/FileSystem/FileSystem.cpp
@@ -907,7 +907,7 @@ std::string FileSystem::GetExtension(const std::string& pathStr)
 }
 
 std::string FileSystem::GetNormalizedPath(const std::string& path) {
-	return Impl::StoreUTF8AsString(std::filesystem::path(path).lexically_normal().generic_u8string());
+	return Impl::StoreUTF8AsString(std::filesystem::path(ForwardSlashes(path)).lexically_normal().generic_u8string());
 }
 
 bool FileSystem::CheckFile(const std::string& file)
