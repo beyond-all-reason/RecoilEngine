f357a73e23363907292798571d1661ac09c15e2f
KyleAnthonyShepherd <44480662+KyleAnthonyShepherd@users.noreply.github.com>
2026-02-24 12:59:46 -0500

    Update LuaSyncedCtrl API
    `nil` is now pass-and-do-not-change-value instead of restore to default engine behavior (-1.0f)

--- rts/Lua/LuaSyncedCtrl.cpp
+++ rts/Lua/LuaSyncedCtrl.cpp
@@ -2798,15 +2798,15 @@ int LuaSyncedCtrl::SetUnitEngagementRange(lua_State* L)
 
 	const int args = lua_gettop(L);
 
-	if (args >= 2) {
-		if (lua_isnoneornil(L, 2) || (lua_isboolean(L, 2)))
+	if (args >= 2 && !lua_isnoneornil(L, 2)) {
+		if (lua_isboolean(L, 2))
 			mCAI->engagementRange = -1.0f;
 		else
 			mCAI->engagementRange = luaL_checkfloat(L, 2);
 	}
 
-	if (args >= 3) {
-		if (lua_isnoneornil(L, 3) || (lua_isboolean(L, 3)))
+	if (args >= 3 && !lua_isnoneornil(L, 3)) {
+		if (lua_isboolean(L, 3))
 			mCAI->fightEngagementRange = -1.0f;
 		else
 			mCAI->fightEngagementRange = luaL_checkfloat(L, 3);
