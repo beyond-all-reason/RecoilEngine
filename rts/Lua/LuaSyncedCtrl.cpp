2a1f5fa87c1b6e337734c333c5ec3789e10d9754
KyleAnthonyShepherd <44480662+KyleAnthonyShepherd@users.noreply.github.com>
2026-02-24 13:08:55 -0500

    Cut out unncessary lua_gettop(L)

--- rts/Lua/LuaSyncedCtrl.cpp
+++ rts/Lua/LuaSyncedCtrl.cpp
@@ -2796,16 +2796,14 @@ int LuaSyncedCtrl::SetUnitEngagementRange(lua_State* L)
 	if (mCAI == nullptr)
 		return 0;
 
-	const int args = lua_gettop(L);
-
-	if (args >= 2 && !lua_isnoneornil(L, 2)) {
+	if (!lua_isnoneornil(L, 2)) {
 		if (lua_isboolean(L, 2))
 			mCAI->engagementRange = -1.0f;
 		else
 			mCAI->engagementRange = luaL_checkfloat(L, 2);
 	}
 
-	if (args >= 3 && !lua_isnoneornil(L, 3)) {
+	if (!lua_isnoneornil(L, 3)) {
 		if (lua_isboolean(L, 3))
 			mCAI->fightEngagementRange = -1.0f;
 		else
