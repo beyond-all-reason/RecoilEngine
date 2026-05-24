9c696cf88a0942439151cffe726ad3268321837c
Wybren Koelmans <wybrenkoelmans@gmail.com>
2026-05-24 15:55:58 +0000

    fix: update DefaultCommand documentation to include command ID parameter

--- rts/Lua/LuaHandle.cpp
+++ rts/Lua/LuaHandle.cpp
@@ -2624,6 +2624,8 @@ void CLuaHandle::SunChanged()
  * @function Callins:DefaultCommand
  * @param type "unit"|"feature" The type of the object pointed at.
  * @param id integer The `unitID` or `featureID`.
+ * @param cmd integer The current command ID.
+ * @return integer The command ID to use as the default, or nil to clear the default.
  */
 bool CLuaHandle::DefaultCommand(const CUnit* unit,
                                 const CFeature* feature, int& cmd)
