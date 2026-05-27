e87fcc144a3598508cd79e15c7f3cd6f3bd1fac5
dargem <genericz145@gmail.com>
2026-05-27 11:46:32 +1000

    Add string literal overloads for LuaPushNamedFoo that enable a compile time hash. Remove legacy hashing macros and replace callsites with corresponding LuaPushNamedFoo

--- rts/System/StringHash.h
+++ rts/System/StringHash.h
@@ -36,4 +36,9 @@ struct compileTimeHasher<length, step, idx, idx> {
 	}
 };
 
-#define COMPILE_TIME_HASH(str) compileTimeHasher<sizeof(str) - 1>::hash(str)
+template <unsigned N>
+[[nodiscard]] constexpr uint32_t LuaHashStringLiteral(const char (&literal)[N]) noexcept
+{
+	static_assert(N > 0);
+	return compileTimeHasher<N - 1>::hash(literal);
+}
