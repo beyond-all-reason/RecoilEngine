785ec25ae70a3e6540c6a115eb8111ed0847612d
Bruno Da Silva <Bruno-DaSilva@users.noreply.github.com>
2026-06-01 09:38:06 +0000

    added a quick comment

--- rts/Sim/MoveTypes/MoveDefHandler.cpp
+++ rts/Sim/MoveTypes/MoveDefHandler.cpp
@@ -336,6 +336,9 @@ MoveDef::MoveDef(const LuaTable& moveDefTable): MoveDef() {
 		waterline = std::abs(moveDefTable.GetInt("waterline", defaultWaterline));
 		overrideUnitWaterline = moveDefTable.GetBool("overrideUnitWaterline", overrideUnitWaterline);
 	} else {
+		// we read as int from configuration but used as float...
+		// this should probably be changed to float everywhere
+		// github issue: https://github.com/beyond-all-reason/RecoilEngine/issues/2996
 		waterline = static_cast<float>(std::numeric_limits<int>::max());
 	}
 
