a14d3ea2fd30916ea689b9f173d29157086377f6
Wybren Koelmans <wybrenkoelmans@gmail.com>
2026-07-10 16:31:42 +0200

    revert unrelated change to PR (I got to get this stuff in master so I don't have to hunt down each changeset for these compiler related changes)

--- rts/System/ForceInline.hpp
+++ rts/System/ForceInline.hpp
@@ -9,9 +9,7 @@
 #define RECOIL_FORCE_INLINE [[gnu::always_inline]] inline
 
 #elif defined(_MSC_VER)
-// Do NOT promote C4714 to an error: under LTCG (/GL, used by RelWithDebInfo)
-// MSVC defers inlining to link time and reports "__forceinline not inlined"
-// harmlessly, which would otherwise abort the link with LNK1257.
+#pragma warning(error: 4714)
 #define RECOIL_FORCE_INLINE __forceinline
 
 #else
