3fbb3dbac9a877c3753a3d534bae2f3186818df7
rlcevg <514675+rlcevg@users.noreply.github.com>
2026-08-05 20:23:26 +0300

    Enforce alignment for std::aligned_alloc

--- rts/System/MemoryOverride.cpp
+++ rts/System/MemoryOverride.cpp
@@ -70,6 +70,7 @@ void* aligned_alloc(size_t alignment, size_t size)
 	#else
 		// std::aligned_alloc requires size to be a multiple of alignment (C11/C++17);
 		// unlike posix_memalign, passing a non-multiple is undefined behaviour
+		size = (size + alignment - 1) & ~(alignment - 1);  // bitwise round-up
 		assert(size % alignment == 0);
 		return std::aligned_alloc(alignment, size);
 	#endif
