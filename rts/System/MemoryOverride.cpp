8469925c92d05f97bf8d870f1095bb6f53519b17
rlcevg <514675+rlcevg@users.noreply.github.com>
2026-08-18 15:43:36 +0300

    Add assumption assert
    Co-authored-by: sprunk <spr.ng@o2.pl>

--- rts/System/MemoryOverride.cpp
+++ rts/System/MemoryOverride.cpp
@@ -71,7 +71,8 @@ void* aligned_alloc(size_t alignment, size_t size)
 		// std::aligned_alloc requires size to be a multiple of alignment (C11/C++17);
 		// unlike posix_memalign, passing a non-multiple is undefined behaviour
 		size = (size + alignment - 1) & ~(alignment - 1);  // bitwise round-up
-		assert(size % alignment == 0);
+		assert(std::has_single_bit(alignment)); // assumption
+		assert(size % alignment == 0); // intention
 		return std::aligned_alloc(alignment, size);
 	#endif
 #endif
