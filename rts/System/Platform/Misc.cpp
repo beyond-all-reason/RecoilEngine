2df6dcc5318769ec193f24ebfdbfd18ecd92124c
Brad Smith <brad@comstyle.com>
2026-09-17 23:00:29 -0400

    Make use of getexecpath() on OpenBSD with GetProcessExecutableFile()
    OpenBSD has added a function to be able to retrieve the
    executable path.

--- rts/System/Platform/Misc.cpp
+++ rts/System/Platform/Misc.cpp
@@ -168,6 +168,7 @@ namespace Platform
 	// Mac OS X:        _NSGetExecutablePath() (man 3 dyld)
 	// Linux:           readlink /proc/self/exe
 	// Solaris:         getexecname()
+	// OpenBSD:         getexecpath()
 	// FreeBSD:         sysctl CTL_KERN KERN_PROC KERN_PROC_PATHNAME -1
 	// BSD with procfs: readlink /proc/curproc/file
 	// Windows:         GetModuleFileName() with hModule = NULL
@@ -209,6 +210,13 @@ namespace Platform
 		char path[PATH_MAX];
 
 		if (_NSGetExecutablePath(path, &pathlen) == 0)
+			procExeFilePath = path;
+
+
+		#elif defined(__OpenBSD__) && defined(HAVE_GETEXECPATH)
+		char path[PATH_MAX];
+
+		if (getexecpath(path, sizeof(path)) == 0)
 			procExeFilePath = path;
 
 
