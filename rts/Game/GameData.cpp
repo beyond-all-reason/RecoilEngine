6e274e7d723bc435cd73ef9945e17d27fc1b43fa
efrec <quantumscrewdriver@gmail.com>
2026-09-25 12:12:26 -0400

    name the NETMSG_GAMEDATA size fields
    Production build compiles the same. Did not test a debug build.
    
    Replaces bare numbers for calculating the header and length fields. The header is a message ID plus the uint16 packet length. Copied from CBaseNetProtocol::Send.
    
    Could also pick up other UnpackPacket call sites.

--- rts/Game/GameData.cpp
+++ rts/Game/GameData.cpp
@@ -32,7 +32,9 @@ GameData::GameData(std::shared_ptr<const RawPacket> pckt)
 	RECOIL_DETAILED_TRACY_ZONE;
 	assert(pckt->data[0] == NETMSG_GAMEDATA);
 
-	UnpackPacket packet(pckt, 3);
+	const uint32_t headerSize = sizeof(uint8_t) + sizeof(uint16_t);
+
+	UnpackPacket packet(pckt, headerSize);
 
 	std::uint16_t compressedSize;
 
@@ -62,9 +64,12 @@ const netcode::RawPacket* GameData::Pack() const
 	assert(!compressed.empty());
 	assert(compressed.size() <= std::numeric_limits<std::uint16_t>::max());
 
-	const std::uint16_t size = 3 + sizeof(mapChecksum) + sizeof(modChecksum) + compressed.size() + 2 + sizeof(uint32_t);
-	PackPacket* buffer = new PackPacket(size, NETMSG_GAMEDATA);
-	*buffer << size;
+	const uint32_t payloadSize = sizeof(uint16_t) + compressed.size() + sizeof(mapChecksum) + sizeof(modChecksum) + sizeof(randomSeed);
+	const uint32_t headerSize = sizeof(uint8_t) + sizeof(uint16_t);
+	const uint16_t packetSize = headerSize + payloadSize;
+
+	PackPacket* buffer = new PackPacket(packetSize, NETMSG_GAMEDATA);
+	*buffer << packetSize;
 	*buffer << std::uint16_t(compressed.size());
 	*buffer << compressed;
 
